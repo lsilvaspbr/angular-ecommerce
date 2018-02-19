94a15a2732916bd3bd9487264fccc07ec78f3934
Lucas Silva <lsilvaspbr@gmail.com>
2018-02-19 00:51:54 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProfileInformationsComponent } from './my-account/profile-informations/profile-informations.component';
+import { AddressListComponent } from './my-account/address-list/address-list.component';
+import { OrderHistoryComponent } from './my-account/order-history/order-history.component';
+import { WishlistComponent } from './my-account/wishlist/wishlist.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileInformationsComponent,
+    AddressListComponent,
+    OrderHistoryComponent,
+    WishlistComponent,
+    MyAccountComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/my-account/profile-informations/profile-informations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-informations',
+  templateUrl: './profile-informations.component.html',
+  styleUrls: ['./profile-informations.component.css']
+})
+export class ProfileInformationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
