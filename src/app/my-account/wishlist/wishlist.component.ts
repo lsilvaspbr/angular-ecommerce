0ec4266030136f176737010abd05599da9a393fa
Lucas Silva <lsilvaspbr@gmail.com>
2018-02-21 22:25:35 -0300

    Forms implementation

--- src/app/my-account/wishlist/wishlist.component.ts
+++ src/app/my-account/wishlist/wishlist.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
+  wishList = [
+    {
+      name: 'Nike Shirt',
+      description: 'Nike FC Barcelona 2018 Breathe Third Soccer Jersey',
+      image: 'assets/barcelonashirt.jpg',
+      date: '2018-01-08'
+    },
+    {
+      name: 'Adidas Soccer Shoes',
+      description: 'Adidas Ace 18.3 FG Soccer Shoes',
+      image: 'assets/adidasboots.jpg',
+      date: '2018-01-20'
+    },
+    {
+      name: 'Nike MercurialX Shoes ',
+      description: 'Nike MercurialX Victory VI IC Indoor Soccer Shoes',
+      image: 'assets/nikeboots.jpg',
+      date: '2018-01-20'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
